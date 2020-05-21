2151f6d65d6b68fee96b9831476f5ba5ef07f325
Dittam Dey <dittam@live.com>
2020-05-20 20:05:15 -0400

    Revert "Revert "added preprocessing script for new kaggle dataset""
    This reverts commit 3837213fd8514464c554fd7063747880e7b9f168.

--- train.py
+++ train.py
@@ -1,17 +1,18 @@
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
-from preprocessing import CornellMovieDataGenerator
+from preprocessing import CustomDataGenerator
 from model import defineModels
 from keras.callbacks import LambdaCallback, ReduceLROnPlateau
 import numpy as np
 
 MAX_SENTENCE_LENGTH = 20
-MAX_VOCAB_SIZE = 10000
+MAX_VOCAB_SIZE = 1879
 LATENT_DIM = 250
 EMBEDDING_DIM = 100
-BATCH_SIZE = 540
-EPOCHS = 20
+BATCH_SIZE = 25
+EPOCHS = 2
 LR_DECAY = 0.2
+path = "C:/Users/ditta/Desktop/kagglePyBotDataset"
 
 
 def on_epoch_end(epoch, logs):
@@ -21,8 +22,10 @@ def on_epoch_end(epoch, logs):
                        dataGenerator.word2idx, MAX_SENTENCE_LENGTH))
   print("")
   if epoch % 5 == 0:
-    eModel.save("trainedModels/eModel{}_{}.h5".format(epoch, logs["loss"]))
-    dModel.save("trainedModels/dModel{}_{}.h5".format(epoch, logs["loss"]))
+    eModel.save(
+      path + "trainedModels/eModel{}_{}.h5".format(epoch, logs["loss"]))
+    dModel.save(
+      path + "trainedModels/dModel{}_{}.h5".format(epoch, logs["loss"]))
 
 
 customCallback = LambdaCallback(on_epoch_end=on_epoch_end)
@@ -31,12 +34,12 @@ reduceLRCallback = ReduceLROnPlateau(
 callbackLst = [customCallback, reduceLRCallback]
 
 
-dataGenerator = CornellMovieDataGenerator(
-    'data/trainInputs.txt', 'data/trainTargets.txt', BATCH_SIZE, MAX_SENTENCE_LENGTH, MAX_VOCAB_SIZE)
-embeddingMatrix = np.load("data/embeddingMatrixGolve6b100.npy")
+dataGenerator = CustomDataGenerator(
+    path + '/trainInputs.txt', path + '/trainTargets.txt', BATCH_SIZE, MAX_SENTENCE_LENGTH, MAX_VOCAB_SIZE)
+embeddingMatrix = np.load(path + "/embeddingMatrixGolve6b100_2.npy")
 print("training...")
 trainModel, eModel, dModel = defineModels(
-  MAX_SENTENCE_LENGTH, MAX_VOCAB_SIZE, LATENT_DIM, EMBEDDING_DIM, LR, embeddingMatrix)
+  MAX_SENTENCE_LENGTH, MAX_VOCAB_SIZE, LATENT_DIM, EMBEDDING_DIM, embeddingMatrix)
 
 trainHist = trainModel.fit_generator(generator=dataGenerator, epochs=EPOCHS,
                                      use_multiprocessing=True, workers=4,
